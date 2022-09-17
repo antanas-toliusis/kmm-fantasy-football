710b289f38b9c5a6726ec9b1f74f2b6e19407257
antanas-toliusis <antanas.toliusis1@gmail.com>
2022-09-17 08:31:20 +0100

    Initialize koala

---
+++ app/src/main/java/dev/johnoreilly/fantasypremierleague/MainActivity.kt
@@ -0,0 +1,144 @@
+package dev.johnoreilly.fantasypremierleague
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
+import androidx.navigation.navArgument
+import dev.johnoreilly.fantasypremierleague.presentation.Screen
+import dev.johnoreilly.fantasypremierleague.presentation.bottomNavigationItems
+import dev.johnoreilly.fantasypremierleague.presentation.fixtures.FixturesListView
+import dev.johnoreilly.fantasypremierleague.presentation.fixtures.fixtureDetails.FixtureDetailsView
+import dev.johnoreilly.fantasypremierleague.presentation.global.FantasyPremierLeagueTheme
+import dev.johnoreilly.fantasypremierleague.presentation.players.PlayerListView
+import dev.johnoreilly.fantasypremierleague.presentation.FantasyPremierLeagueViewModel
+import dev.johnoreilly.fantasypremierleague.presentation.leagues.LeagueListView
+import dev.johnoreilly.fantasypremierleague.presentation.players.playerDetails.PlayerDetailsView
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity : AppCompatActivity() {
+    private val fantasyPremierLeagueViewModel: FantasyPremierLeagueViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FantasyPremierLeagueTheme {
+                MainLayout(fantasyPremierLeagueViewModel)
+            }
+        }
+    }
+}
+
+@Composable
+fun MainLayout(viewModel: FantasyPremierLeagueViewModel) {
+    val navController = rememberNavController()
+    val coroutineScope = rememberCoroutineScope()
+
+    val leagues by viewModel.leagues.collectAsState()
+
+    coroutineScope.launch {
+        // TEMP to set a particular league until settings screen added
+        //viewModel.updateLeagues(listOf(""))
+    }
+
+    Scaffold(bottomBar = { FantasyPremierLeagueBottomNavigation(navController, leagues) }) {
+
+        NavHost(navController, startDestination = Screen.PlayerListScreen.title, modifier = Modifier.padding(it)) {
+            composable(Screen.PlayerListScreen.title) {
+                PlayerListView(
+                    fantasyPremierLeagueViewModel = viewModel,
+                    onPlayerSelected = { playerId ->
+                        navController.navigate(Screen.PlayerDetailsScreen.title + "/${playerId}")
+                    }
+                )
+            }
+            composable(
+                Screen.PlayerDetailsScreen.title + "/{playerId}",
+                arguments = listOf(navArgument("playerId") { type = NavType.IntType })
+            ) { navBackStackEntry ->
+                val playerId: Int? = navBackStackEntry.arguments?.getInt("playerId")
+                playerId?.let {
+                    val player = viewModel.getPlayer(playerId)
+                    player?.let {
+                        PlayerDetailsView(viewModel, player, popBackStack = { navController.popBackStack() })
+                    }
+                }
+            }
+            composable(Screen.FixtureListScreen.title) {
+                FixturesListView(
+                    fantasyPremierLeagueViewModel = viewModel,
+                    onFixtureSelected = { fixtureId ->
+                        navController.navigate(Screen.FixtureDetailsScreen.title + "/${fixtureId}")
+                    }
+                )
+            }
+            composable(
+                Screen.FixtureDetailsScreen.title + "/{fixtureId}",
+                arguments = listOf(navArgument("fixtureId") { type = NavType.IntType })
+            ) { navBackStackEntry ->
+                val fixtureId: Int? = navBackStackEntry.arguments?.getInt("fixtureId")
+                fixtureId?.let {
+                    val fixture = viewModel.getFixture(fixtureId)
+                    fixture?.let {
+                        FixtureDetailsView(fixture, popBackStack = { navController.popBackStack() })
+                    }
+                }
+            }
+            composable(Screen.LeagueStandingsListScreen.title) {
+                LeagueListView(
+                    viewModel = viewModel
+                )
+            }
+
+        }
+    }
+}
+
+
+@Composable
+private fun FantasyPremierLeagueBottomNavigation(navController: NavHostController, leagues: List<String>) {
+
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        bottomNavigationItems.forEach { bottomNavigationitem ->
+
+            val skipItem =  bottomNavigationitem.route == Screen.LeagueStandingsListScreen.title
+                    && leagues.isEmpty()
+
+            if (!skipItem) {
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            bottomNavigationitem.icon,
+                            contentDescription = bottomNavigationitem.iconContentDescription
+                        )
+                    },
+                    selected = currentRoute == bottomNavigationitem.route,
+                    onClick = {
+                        navController.navigate(bottomNavigationitem.route) {
+                            popUpTo(navController.graph.id)
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+        }
+    }
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
+        classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:${Versions.kmpNativeCoroutinesVersion}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+
+allprojects {
+    configurations.all {
+        resolutionStrategy.dependencySubstitution {
+            substitute(module("org.jetbrains.compose.compiler:compiler")).apply {
+                using(module("androidx.compose.compiler:compiler:${Versions.composeCompiler}"))
+            }
+        }
+    }
+}
