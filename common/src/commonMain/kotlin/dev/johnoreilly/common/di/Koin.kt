2f9d260c4c99f4d87e902a379c294671783f094d
antanas-toliusis <antanas.toliusis1@gmail.com>
2022-09-17 08:31:14 +0100

    Initialize kotlin_1_6_20

---
+++ common/src/commonMain/kotlin/dev/johnoreilly/common/di/Koin.kt
@@ -0,0 +1,57 @@
+package dev.johnoreilly.common.di
+
+import dev.johnoreilly.common.data.remote.FantasyPremierLeagueApi
+import dev.johnoreilly.common.data.repository.FantasyPremierLeagueRepository
+import dev.johnoreilly.common.data.repository.FixtureDb
+import dev.johnoreilly.common.data.repository.PlayerDb
+import dev.johnoreilly.common.data.repository.TeamDb
+import dev.johnoreilly.common.platformModule
+import io.realm.Configuration
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import io.ktor.client.*
+import io.ktor.client.engine.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.module
+
+fun initKoin(enableNetworkLogs: Boolean = false, appDeclaration: KoinAppDeclaration = {}) =
+    startKoin {
+        appDeclaration()
+        modules(commonModule(enableNetworkLogs = enableNetworkLogs), platformModule())
+    }
+
+// called by iOS etc
+fun initKoin() = initKoin(enableNetworkLogs = false) {}
+
+fun commonModule(enableNetworkLogs: Boolean) = module {
+    single { createJson() }
+    single { createHttpClient(get(), get(), enableNetworkLogs = enableNetworkLogs) }
+
+    single<Configuration> { RealmConfiguration.with(schema = setOf(PlayerDb::class, TeamDb::class, FixtureDb::class)) }
+    single { Realm.open(get()) }
+
+    single { FantasyPremierLeagueRepository() }
+    single { FantasyPremierLeagueApi(get()) }
+}
+
+fun createJson() = Json { isLenient = true; ignoreUnknownKeys = true
+    // to avoid 'mutation attempt of frozen kotlin.collections.HashMap@' in K/N
+    useAlternativeNames = false
+}
+
+fun createHttpClient(httpClientEngine: HttpClientEngine, json: Json, enableNetworkLogs: Boolean) = HttpClient(httpClientEngine) {
+    install(ContentNegotiation) {
+        json(json)
+    }
+    if (enableNetworkLogs) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.INFO
+        }
+    }
+}
